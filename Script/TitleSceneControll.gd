ab4a875dea3086bc607c29a5f5855af87abe2eda
bootcat123shoecat123 <oooth16@gmail.com>
2024-06-08 18:40:27 +0800

    ArmyUnLine Title

--- Script/TitleSceneControll.gd
+++ Script/TitleSceneControll.gd
@@ -4,7 +4,8 @@ extends Button
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+@onready var animation_Texture:TextureRect=$"../../AnimationPlayer/TextureRect"
+@onready var animation_Player:AnimationPlayer=$"../../AnimationPlayer"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,6 +14,8 @@ func _ready():
 func _end_game():
 	get_tree().quit();
 func _start_game():
-	get_tree().change_scene_to_file("res://Scenes/new scene.tscn");
+	animation_Texture.visible=true
+	animation_Player.queue("fade_out")
+	get_tree().change_scene_to_file("res://Scenes/Level_001.tscn");
 func _setting():
 	print("setting");
